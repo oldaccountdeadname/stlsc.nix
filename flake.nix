c5933f57842c821a30ab93b15cdd153fc4d14036
lincoln auster [they/them] <lincolnauster@gmail.com>
2021-11-23 10:20:38 -0700

    extract generation to file tls.nix
    This is step two in allowing overrides: we define the abstract type
    metadata and require it to provide the information we need for generation.
    flake.nix just provides some default options.

--- flake.nix
+++ flake.nix
@@ -6,51 +6,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        country = "NA";
-        state   = "SomeStateThatDefinitelyExists";
-        city    = "SomeCityThatDefinitelyExists";
-        org     = "SomeRealOrganization";
-        orgunit = "SomeRealOrganizationalUnit";
-        fqdn    = "a.real.fqdn";
-        email   = "anemail@thatis.real";
-        pass    = "iamcommitingcrimes";
-      in {
-        defaultPackage = pkgs.stdenv.mkDerivation {
-          name = "stlsc";
-          version = "0.1.0";
-          src = ./.;
-
-          buildPhase = ''
-            cat <<EOF | openssl req -x509 -newkey rsa:4096 -sha256 \
-                                -passout 'pass:${pass}'            \
-                                -keyout  privkey.pem               \
-                                -out     tlscert.pem
-            ${country}
-            ${state}
-            ${city}
-            ${org}
-            ${orgunit}
-            ${fqdn}
-            ${email}
-            EOF
-
-            openssl pkcs12 -export -in tlscert.pem -inkey privkey.pem \
-                    -password 'pass:${pass}' \
-                    -passin   'pass:${pass}' \
-                    -passout  'pass:${pass}' \
-                    -out tlscert.pfx
-          '';
-
-          installPhase = ''
-            mkdir $out
-            echo ${pass} > $out/pass
-            cp privkey.pem $out
-            cp tlscert.pem $out
-            cp tlscert.pfx $out
-          '';
-
-          nativeBuildInputs = [ pkgs.openssl ];
+        tlsgen = import ./tls.nix;
+        metadata = {
+          country = "NA";
+          state   = "SomeStateThatDefinitelyExists";
+          city    = "SomeCityThatDefinitelyExists";
+          org     = "SomeRealOrganization";
+          orgunit = "SomeRealOrganizationalUnit";
+          fqdn    = "a.real.fqdn";
+          email   = "anemail@thatis.real";
+          pass    = "iamcommitingcrimes";
         };
+      in {
+        defaultPackage = tlsgen pkgs metadata;
       }
     );
 }
